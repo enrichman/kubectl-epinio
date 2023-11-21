d79c85709d4ecb82f02f4c1f1507aff0124b625d
Enrico Candino <enrico.candino@suse.com>
2023-11-21 07:38:07 +0100

    refactor tests

---
+++ tests/delete_users_test.go
@@ -0,0 +1,45 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeleteUser(t *testing.T) {
+	epinio, err := NewKubectlEpinio()
+	assert.NoError(t, err)
+
+	_, stderr, err := epinio.Create("user", "foo")
+	require.NoError(t, err, stderr)
+
+	// check user exists
+	stdout, _, err := epinio.Get("user", "foo")
+	require.NoError(t, err)
+	outTable := parseOutTable(stdout)
+	assert.Equal(t, "foo", outTable[1][0])
+
+	_, _, err = epinio.Delete("user", "foo")
+	assert.NoError(t, err, stderr)
+
+	// check user do not exists
+	stdout, _, err = epinio.Get("user", "foo")
+	require.NoError(t, err)
+	outTable = parseOutTable(stdout)
+	assert.Equal(t, 1, len(outTable))
+}
+
+func TestDeleteNonExistingUser(t *testing.T) {
+	epinio, err := NewKubectlEpinio()
+	assert.NoError(t, err)
+
+	// check user do not exists
+	stdout, _, err := epinio.Get("user", "foo")
+	require.NoError(t, err)
+	outTable := parseOutTable(stdout)
+	assert.Equal(t, 1, len(outTable))
+
+	_, _, err = epinio.Delete("user", "foo")
+	assert.Error(t, err)
+}
--- tests/get_test.go
+++ tests/get_test.go
@@ -6,11 +6,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDelete(t *testing.T) {
+func TestGet(t *testing.T) {
 	epinio, err := NewKubectlEpinio()
 	assert.NoError(t, err)
 
-	out, _, err := epinio.Run("delete")
+	out, _, err := epinio.Run("get")
 	assert.NoError(t, err)
 
 	assert.Contains(t, out, "Usage:")
