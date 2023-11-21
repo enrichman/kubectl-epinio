d550320070d8205a41f19cff832e07ab0d7d5340
Enrico Candino <enrico.candino@suse.com>
2023-11-21 07:52:16 +0100

    fix tests delete

--- tests/delete_roles_test.go
+++ tests/delete_roles_test.go
@@ -20,14 +20,14 @@ func TestDeleteRole(t *testing.T) {
 	outTable := parseOutTable(stdout)
 	assert.Equal(t, "foo", outTable[1][0])
 
-	_, _, err = epinio.Delete("role", "foo")
-	assert.NoError(t, err)
+	_, _, err = epinio.Delete("role", "foo", "-y")
+	require.NoError(t, err)
 
 	// check role do not exists
 	stdout, _, err = epinio.Get("role", "foo")
 	require.NoError(t, err)
 	outTable = parseOutTable(stdout)
-	assert.Equal(t, 1, len(outTable))
+	assert.Equal(t, 1, len(outTable), stdout)
 }
 
 func TestDeleteNonExistingRole(t *testing.T) {
--- tests/delete_users_test.go
+++ tests/delete_users_test.go
@@ -20,7 +20,7 @@ func TestDeleteUser(t *testing.T) {
 	outTable := parseOutTable(stdout)
 	assert.Equal(t, "foo", outTable[1][0])
 
-	_, _, err = epinio.Delete("user", "foo")
+	_, _, err = epinio.Delete("user", "foo", "-y")
 	assert.NoError(t, err, stderr)
 
 	// check user do not exists
@@ -38,7 +38,7 @@ func TestDeleteNonExistingUser(t *testing.T) {
 	stdout, _, err := epinio.Get("user", "foo")
 	require.NoError(t, err)
 	outTable := parseOutTable(stdout)
-	assert.Equal(t, 1, len(outTable))
+	assert.Equal(t, 1, len(outTable), stdout)
 
 	_, _, err = epinio.Delete("user", "foo")
 	assert.Error(t, err)
